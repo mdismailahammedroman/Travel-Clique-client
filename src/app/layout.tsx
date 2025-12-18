0eac61b8df819b1b8aa459f2ea23a1eb54e3813c
unknown <ismailahmmedroman@gmail.com>
2025-12-18 15:57:16 +0600

    redux setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "@/lib/redux/store";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,9 +26,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
         {/* Wrap all pages in CommonLayout */}
-      {children}
+        <ReduxProvider store={store}>{children}</ReduxProvider>
       </body>
     </html>
   );
